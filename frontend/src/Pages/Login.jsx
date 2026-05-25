d43728ad28448bb1e74d9ece2db8bcec0d8e7efb
0mairAftab <omairch035@gmail.com>
2026-05-25 09:57:12 +0500

    aadded functionality to redirect authenticated users to home page if they try to access login or signup page

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 import Login from '../Components/Login/Login'
+import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 const LoginPage = () => {
+
+  const navigate= useNavigate();
+  const { isAuthenticated } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/");  // If the user is already authenticated, we redirect them to the home page. This prevents logged-in users from accessing the login page, which is a common practice to enhance user experience and security.
+    }
+  }, [])
+
+
   return (
     <div>
         <Login />
--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import SignUp from '../Components/SignUp/SignUp.jsx'
+import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const SignUpPage = () => {
+
+  const navigate= useNavigate();
+  const { isAuthenticated } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/");  // If the user is already authenticated, we redirect them to the home page. This prevents logged-in users from accessing the login page, which is a common practice to enhance user experience and security.
+    }
+  }, [])
+
   return (
     <div>
         <SignUp/>
