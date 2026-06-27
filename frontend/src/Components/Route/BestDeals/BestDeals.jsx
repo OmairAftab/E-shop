d2d6a9f053a13f617858655865f0d67d848995e8
0mairAftab <omairch035@gmail.com>
2026-06-27 13:00:37 +0500

    updated bestdeals and featuredproduct section that if dynamic data is avialble that use that otherwise use staticdata

--- frontend/src/Components/Route/BestDeals/BestDeals.jsx
+++ frontend/src/Components/Route/BestDeals/BestDeals.jsx
@@ -2,17 +2,21 @@ import React, { useEffect, useState } from 'react'
 import { productData } from '../../../Static/data'
 import styles from '../../../Styles/styles'
 import ProductCard from '../ProductCard/ProductCard'
+import { useSelector } from 'react-redux'
 
 const BestDeals = () => {
 
     const [data,setData]=useState([])
 
 
-
+    const {products, isLoading} = useSelector((state) => state.products);
+  const featuredProducts = products && products.length !== 0 ? products : productData; //mean agar dynamic data hai to wo show kra do
+                                                                                      //agar nhi hto static data show krao
 
     useEffect(()=>{
 
-        const d=productData && productData.sort((a,b)=>b.total_sell-a.total_sell)
+        const dddata= products && products.length !==0 ? products : productData;  //mean agar dynamic data hai to wo show kra do nhi to static data show krao
+        const d=dddata && dddata.sort((a,b)=>(b.sold_out || b.total_sell)-(a.sold_out || a.total_sell))
         const firstFive=d.slice(0,5)
         setData(firstFive)
 
--- frontend/src/Components/Route/FeaturedProducts/FeaturedProducts.jsx
+++ frontend/src/Components/Route/FeaturedProducts/FeaturedProducts.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import { productData } from '../../../Static/data'
 import styles from '../../../Styles/styles'
 import ProductCard from '../ProductCard/ProductCard'
+import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { productData } from '../../../Static/data'
 
 const FeaturedProducts = () => {
+
+  const {products, isLoading} = useSelector((state) => state.products);
+  const featuredProducts = products && products.length !== 0 ? products : productData; //mean agar dynamic data hai to wo show kra do
+                                                                                      //agar nhi hto static data show krao
+
+  useEffect(()=>{
+    console.log("products in FeaturedProducts:", featuredProducts);
+  },[featuredProducts])
+
   return (
+    
     <div className='mt-7'>
       <div className={`${styles.section}`}>
         <div className={`${styles.heading}`}>
@@ -13,11 +25,13 @@ const FeaturedProducts = () => {
 
             <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5">
             
-            {
-                productData && productData.map((i,index)=>(
-                    <ProductCard data={i} key={index} />
-                ))
-            } 
+             {
+            featuredProducts && featuredProducts.length !== 0 &&(
+              <>
+               {featuredProducts && featuredProducts.map((i, index) => <ProductCard data={i} key={index} />)}
+              </>
+            )
+           } 
 
             </div>
 
