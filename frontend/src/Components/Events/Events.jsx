611668b5085437d9ff6ae88d6414b6b1d7d3e95c
0mairAftab <omairch035@gmail.com>
2026-06-28 19:18:55 +0500

    Under the Popular Event heading on homepage we are showing that event with most sold out items

--- frontend/src/Components/Events/Events.jsx
+++ frontend/src/Components/Events/Events.jsx
@@ -1,26 +1,35 @@
 import React from 'react'
 import styles from '../../Styles/styles'
 import EventCard from '../Events/EventCard'
+import { useSelector } from 'react-redux'
 
 const Events = () => {
+  const { allEvents, isLoading } = useSelector((state) => state.events);
+
+   // find the event with the highest sold_out count among all events wo hum homepage pe show karenge
+  const mostSoldEvent =
+    allEvents && allEvents.length !== 0
+      ? allEvents.reduce((best, current) =>
+          (current.sold_out || 0) > (best.sold_out || 0) ? current : best
+        )
+      : null;
+
   return (
-    <div className='mt-7'>
+    <div>
+      {!isLoading && allEvents && allEvents.length !== 0 && (
+        <div className='mt-7'>
           <div className={`${styles.section}`}>
             <div className={`${styles.heading}`}>
               <h1 className='text-center'>Popular Event</h1>
             </div>
 
-
             <div className="w-full grid">
-
-                <EventCard/>
+              <EventCard data={mostSoldEvent} />
             </div>
-    
-                
-    
-    
-         </div>
+          </div>
         </div>
+      )}
+    </div>
   )
 }
 
