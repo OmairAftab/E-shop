850bb7808b90502e3e5ac631c44a03d0e8d9c4d8
0mairAftab <omairch035@gmail.com>
2026-07-03 10:49:46 +0500

    showed the reviews of the product in that product detail page

--- frontend/src/Components/Products/ProductDetail.jsx
+++ frontend/src/Components/Products/ProductDetail.jsx
@@ -10,6 +10,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { addToWishlist, removeFromWishlist } from '../../redux/actions/wishlist';
 import { toast } from 'react-toastify';
 import { useEffect } from 'react';
+import Ratings from '../Products/Ratings';
 import { addTocart } from '../../redux/actions/cart';
 
 const resolveImageUrl = (image) => {
@@ -382,13 +383,33 @@ const ProductDetailInfo= ({data})=>{
 
 
             {/* PRODUCT REVIEWS TAB */}
-            {
-                active === 2 ? (
-                    <div className="w-full py-8 flex items-center justify-center">
-                        <h5 className="text-[18px]">No reviews yet!</h5>
-                    </div>
-                ) : null
-            }
+        {active === 2 ? (
+        <div className="w-full min-h-[40vh] flex flex-col items-center py-3 overflow-y-scroll">
+          {data &&
+            data.reviews.map((item, index) => (
+              <div className="w-full flex my-2">
+                <img
+                  src={`${item.user.avatar?.url}`}
+                  alt=""
+                  className="w-[50px] h-[50px] rounded-full"
+                />
+                <div className="pl-2 ">
+                  <div className="w-full flex items-center">
+                    <h1 className="font-[500] mr-3">{item.user.name}</h1>
+                    <Ratings rating={data?.ratings} />
+                  </div>
+                  <p>{item.comment}</p>
+                </div>
+              </div>
+            ))}
+
+          <div className="w-full flex justify-center">
+            {data && data.reviews.length === 0 && (
+              <h5>No Reviews have for this product!</h5>
+            )}
+          </div>
+        </div>
+      ) : null}
 
 
 
