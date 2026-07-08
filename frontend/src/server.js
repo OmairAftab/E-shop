03c6897aa0048c56dd10cdc6d5bfac7c387348e7
0mairAftab <omairch035@gmail.com>
2026-07-08 12:11:51 +0500

    update backend URL to render deployment

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8000"
+export const server = "https://e-shop-backend-vgtu.onrender.com"
 
-export const backend_url="http://localhost:8000/"
+export const backend_url="https://e-shop-backend-vgtu.onrender.com/"
